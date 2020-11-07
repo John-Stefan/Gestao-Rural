f2c6e3a8dd1473e6936886591a59b0413cf5b6d6
John-Stefan <johnstn1545@hotmail.com>
2020-11-07 12:39:08 -0300

    GR0007 - Implementação de Componentes

--- Front-End/bovinainfoFront/src/app/app.module.ts
+++ Front-End/bovinainfoFront/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroFuncionarioComponent } from './cadastro-funcionario/cadastro-funcionario.component';
+import { EmissaoFuncionarioComponent } from './emissao-funcionario/emissao-funcionario.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastroFuncionarioComponent,
+    EmissaoFuncionarioComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- Front-End/bovinainfoFront/src/app/data.service.ts
+++ Front-End/bovinainfoFront/src/app/data.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
-  
+
+  contacts = [
+    { id: 1, name: "João da Silva", description: "Cliente Vip", email: "c001@email.com" },
+    { id: 2, name: "Maria das Dores", description: "Entrou em contato pelo app", email: "c002@email.com" },
+    { id: 3, name: "Pedro Pedreira", description: "Pediu cancelamento", email: "c003@email.com" },
+    { id: 4, name: "Morgana Adams", description: "Deseja comprar uma casa", email: "c004@email.com" }
+  ];
+
   constructor() { }
+
+  public getContacts(): Array<{ id, name, description, email }> {
+    return this.contacts;
+  }
+  public createContact(contact: { id, name, description, email }) {
+    this.contacts.push(contact);
+  }
 }
