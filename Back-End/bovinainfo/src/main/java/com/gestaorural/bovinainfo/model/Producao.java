e4d27e34267dfd7f4d563410db9b02a4826e08ed
John-Stefan <johnstn1545@hotmail.com>
2020-11-19 13:30:24 -0300

    GR0056 - Correção da tabela produção

--- Back-End/bovinainfo/src/main/java/com/gestaorural/bovinainfo/model/Producao.java
+++ Back-End/bovinainfo/src/main/java/com/gestaorural/bovinainfo/model/Producao.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,8 +17,8 @@ public class Producao {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
+	private int id;	
+
 	@Column(name = "data")
 	private Calendar data;
 	
@@ -25,5 +27,49 @@ public class Producao {
 	
 	@Column(name = "umidade")
 	private int umidade;
+	
+	@ManyToOne
+	@JoinColumn(name = "pro_fk_pes")
+	private Pessoa pessoa;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(int temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public int getUmidade() {
+		return umidade;
+	}
+
+	public void setUmidade(int umidade) {
+		this.umidade = umidade;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
 }
 
