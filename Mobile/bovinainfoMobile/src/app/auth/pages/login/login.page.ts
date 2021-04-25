801e0a768499e267fb217210bdfb567d77b41472
John-Stefan <johnstn1545@hotmail.com>
2021-04-25 13:27:15 -0300

    Validação do formulario login

--- Mobile/bovinainfoMobile/src/app/auth/pages/login/login.page.ts
+++ Mobile/bovinainfoMobile/src/app/auth/pages/login/login.page.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Entrar',
+    actionChange: 'Criar uma nova conta'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get name(): FormControl {
+    return <FormControl>this.authForm.get('name');
+  }
+
+  get email(): FormControl {
+    return <FormControl>this.authForm.get('email');
+  }
+
+  get password(): FormControl {
+    return <FormControl>this.authForm.get('password');
+  }
+
+  public changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Entrar' : 'Cadastrar-se';
+    this.configs.actionChange = isSignIn ? 'Criar uma nova conta' : 'Já tenho uma conta';
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
+  }
+
+  public onSubmit(): void {
+    console.log('AuthForm: ', this.authForm.value);
+  }
 }
--- Mobile/bovinainfoMobile/src/app/shared/shared.module.ts
+++ Mobile/bovinainfoMobile/src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [],
   exports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
   ]
 })
