7d7c308375ecc81e8bbbcc0f882bbcd282ae03f4
John-Stefan <johnstn1545@hotmail.com>
2021-04-25 20:15:25 -0300

    Implementação da estrutura de autenticação com Email e Facebook

---
+++ Mobile/bovinainfoMobile/src/app/core/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase';
+import { AuthOptions, AuthProvider, User } from './auth.types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  public authenticate({ isSignIn, provider, user }: AuthOptions): Promise<firebase.auth.UserCredential> {
+    let operation: Promise<firebase.auth.UserCredential>;
+
+    if (provider !== AuthProvider.Email) {
+      operation = this.signInWithPopup(provider);
+    } else {
+      operation = isSignIn ? this.signInWithEmail(user) : this.signUpWithEmail(user);
+    }
+
+    return operation;
+  }
+
+  private signInWithEmail({ email, password }: User): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  private signUpWithEmail({ email, password, name }: User): Promise<firebase.auth.UserCredential> {
+    return this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(credentials =>
+        credentials.user
+          .updateProfile({ displayName: name, photoURL: null })
+          .then(() => credentials)
+      );
+  }
+
+  private signInWithPopup(provider: AuthProvider): Promise<firebase.auth.UserCredential> {
+    let signInProvider = null;
+
+    switch (provider) {
+      case AuthProvider.Facebook:
+        signInProvider = new firebase.auth.FacebookAuthProvider();
+        break;
+    }
+
+    return this.afAuth.signInWithPopup(signInProvider);
+  }
+}
---
+++ Mobile/bovinainfoMobile/src/app/core/services/auth.types.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export enum AuthProvider {
+  Email,
+  Facebook
+}
+
+export interface User {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthOptions {
+  isSignIn: boolean;
+  provider: AuthProvider;
+  user: User;
+}
