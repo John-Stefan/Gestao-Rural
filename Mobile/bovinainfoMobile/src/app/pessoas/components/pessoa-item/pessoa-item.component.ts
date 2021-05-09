8348a6785f8c2739dcd1813e88b33497f667eb1f
John-Stefan <johnstn1545@hotmail.com>
2021-05-09 13:56:12 -0300

    Criação de eventos do CRUD no PessoaItem-Component

--- Mobile/bovinainfoMobile/src/app/pessoas/components/pessoa-item/pessoa-item.component.ts
+++ Mobile/bovinainfoMobile/src/app/pessoas/components/pessoa-item/pessoa-item.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Pessoa } from '../../models/pessoa.model';
 
 @Component({
   selector: 'app-pessoa-item',
   templateUrl: './pessoa-item.component.html',
   styleUrls: ['./pessoa-item.component.scss'],
 })
-export class PessoaItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class PessoaItemComponent {
+  @Input() pessoa: Pessoa;
+  @Output() update = new EventEmitter<Pessoa>();
+  @Output() delete = new EventEmitter<Pessoa>();
 }
