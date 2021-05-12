344e4ef097691e5b0c7526bf380d9060bdf7ca33
John-Stefan <johnstn1545@hotmail.com>
2021-05-12 17:37:00 -0300

    Aplicação da herança do Firestore na Pessoa Service

--- Mobile/bovinainfoMobile/src/app/pessoas/services/pessoas.service.ts
+++ Mobile/bovinainfoMobile/src/app/pessoas/services/pessoas.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Firestore } from 'src/app/core/classes/firestore.class';
+import { Pessoa } from '../models/pessoa.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PessoasService {
+export class PessoasService extends Firestore<Pessoa> {
 
-  constructor() { }
+  constructor(db: AngularFirestore) {
+    super(db);
+  }
 }
