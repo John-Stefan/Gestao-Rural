b4baac0f2b3a1202a24d6157798dae5725250f99
John-Stefan <johnstn1545@hotmail.com>
2021-05-12 16:23:41 -0300

    Teste do componente de listagem

--- Mobile/bovinainfoMobile/src/app/pessoas/pages/pessoas-list/pessoas-list.page.ts
+++ Mobile/bovinainfoMobile/src/app/pessoas/pages/pessoas-list/pessoas-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pessoa } from '../../models/pessoa.model';
 
 @Component({
   selector: 'app-pessoas-list',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoasListPage implements OnInit {
 
+  pessoas$: Observable<Pessoa[]>;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.pessoas$ = of([
+      {
+        id: '1',
+        nome: 'Gilcilene',
+        telefone: '62984758833',
+        cpf: '23498764522',
+        dataNascimento: '20/02/2000',
+      },
+      {
+        id: '2',
+        nome: 'Jerfesson',
+        telefone: '64892387264',
+        cpf: '098643732312',
+        dataNascimento: '11/02/1995',
+      },
+      {
+        id: '3',
+        nome: 'Isabella',
+        telefone: '62985762856',
+        cpf: '09725367482',
+        dataNascimento: '10/10/1990',
+      }
+    ]);
   }
 
 }
