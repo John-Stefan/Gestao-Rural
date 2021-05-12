42cc2e090e7b5e60d06676c25f42bf709e4bfa14
John-Stefan <johnstn1545@hotmail.com>
2021-05-12 16:50:27 -0300

    Implementação da persistencia generica para setar e listar

--- Mobile/bovinainfoMobile/src/app/core/classes/firestore.class.ts
+++ Mobile/bovinainfoMobile/src/app/core/classes/firestore.class.ts
@@ -1,3 +1,19 @@
+/* eslint-disable @typescript-eslint/member-ordering */
+/* eslint-disable @typescript-eslint/quotes */
+import { AngularFirestore, AngularFirestoreCollection, QueryFn } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+
 export abstract class Firestore<T> {
 
+  protected collection: AngularFirestoreCollection<T>;
+
+  constructor(protected db: AngularFirestore) { }
+
+  protected setCollection(path: string, queryFn?: QueryFn): void {
+    this.collection = path ? this.db.collection(path, queryFn) : null;
+  }
+
+  public getAll(): Observable<T[]> {
+    return this.collection.valueChanges();
+  }
 }
