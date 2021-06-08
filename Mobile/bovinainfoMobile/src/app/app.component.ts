3f5703b953c4a867149e86552b1443428240478d
John-Stefan <johnstn1545@hotmail.com>
2021-06-08 00:30:25 -0300

    Criação do Menu

--- Mobile/bovinainfoMobile/src/app/app.component.ts
+++ Mobile/bovinainfoMobile/src/app/app.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/member-delimiter-style */
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor() { }
+
 }
