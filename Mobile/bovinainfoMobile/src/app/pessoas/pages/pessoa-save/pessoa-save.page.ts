24bee85ac506216305c8deb69f35227f82dad382
John-Stefan <johnstn1545@hotmail.com>
2021-06-07 22:49:09 -0300

    Implementação da funcionalidade de emissão

--- Mobile/bovinainfoMobile/src/app/pessoas/pages/pessoa-save/pessoa-save.page.ts
+++ Mobile/bovinainfoMobile/src/app/pessoas/pages/pessoa-save/pessoa-save.page.ts
@@ -1,7 +1,10 @@
+/* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { take } from 'rxjs/operators';
 import { OverlayService } from 'src/app/core/services/overlay.service';
 import { PessoasService } from '../../services/pessoas.service';
 
@@ -11,15 +14,41 @@ import { PessoasService } from '../../services/pessoas.service';
   styleUrls: ['./pessoa-save.page.scss'],
 })
 export class PessoaSavePage implements OnInit {
-
   pessoaForm: FormGroup;
+  pageTitle = '...';
+  pessoaId: string = undefined;
 
   constructor(
-    private fb: FormBuilder, private pessoasService: PessoasService, private navCtrl: NavController, private overlayService: OverlayService
+    private fb: FormBuilder, private pessoasService: PessoasService, private navCtrl: NavController, private overlayService: OverlayService, private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.createForm();
+    this.init();
+  }
+
+  init(): void {
+    const pessoaId = this.route.snapshot.paramMap.get('id');
+    if (!pessoaId) {
+      this.pageTitle = 'Cadastro de Pessoa';
+      return;
+    }
+    this.pessoaId = pessoaId;
+    console.log('pessoaId: ', pessoaId);
+    this.pageTitle = 'Emissão de Pessoa';
+    this.pessoasService.get(pessoaId)
+      .pipe(take(1))
+      .subscribe(({ nome, cpf, dataNascimento, email, telefone, cep, logradouro, complemento, numero }) => {
+        this.pessoaForm.get('nome').setValue(nome);
+        this.pessoaForm.get('cpf').setValue(cpf);
+        this.pessoaForm.get('dataNascimento').setValue(dataNascimento);
+        this.pessoaForm.get('email').setValue(email);
+        this.pessoaForm.get('telefone').setValue(telefone);
+        this.pessoaForm.get('cep').setValue(cep);
+        this.pessoaForm.get('logradouro').setValue(logradouro);
+        this.pessoaForm.get('complemento').setValue(complemento);
+        this.pessoaForm.get('numero').setValue(numero);
+      });
   }
 
   private createForm(): void {
