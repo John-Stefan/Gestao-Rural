c888aa425437eecb19854520408f3b3860dee426
John-Stefan <johnstn1545@hotmail.com>
2021-06-09 14:35:51 -0300

    Implementação dos Inputs e Outputs do componente Pessoa-Item

--- Mobile/bovinainfoMobile/src/app/patrimonios/components/patrimonio-item/patrimonio-item.component.ts
+++ Mobile/bovinainfoMobile/src/app/patrimonios/components/patrimonio-item/patrimonio-item.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Patrimonio } from '../../models/patrimonio.model';
 
 @Component({
   selector: 'app-patrimonio-item',
   templateUrl: './patrimonio-item.component.html',
   styleUrls: ['./patrimonio-item.component.scss'],
 })
-export class PatrimonioItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class PatrimonioItemComponent {
+  @Input() patrimonio: Patrimonio;
+  @Output() update = new EventEmitter<Patrimonio>();
+  @Output() delete = new EventEmitter<Patrimonio>();
 }
+
