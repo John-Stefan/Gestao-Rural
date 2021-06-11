7cc0a1394f245189c25b84850dab23ecbadf2cb4
John-Stefan <johnstn1545@hotmail.com>
2021-06-11 00:30:12 -0300

    Criação da model do Registro

--- Mobile/bovinainfoMobile/src/app/registros/pages/registros-list/registros-list.page.ts
+++ Mobile/bovinainfoMobile/src/app/registros/pages/registros-list/registros-list.page.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-registros-list',
   templateUrl: './registros-list.page.html',
   styleUrls: ['./registros-list.page.scss'],
 })
-export class RegistrosListPage implements OnInit {
+export class RegistrosListPage {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
