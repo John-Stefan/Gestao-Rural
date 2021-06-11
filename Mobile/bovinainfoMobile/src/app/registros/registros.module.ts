9394f1909de7fea4f8b7020178fd7bbb8a8356de
John-Stefan <johnstn1545@hotmail.com>
2021-06-11 00:34:36 -0300

    Criação do Routing do registro

---
+++ Mobile/bovinainfoMobile/src/app/registros/registros.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { RegistrosRoutingModule } from './registros-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RegistrosRoutingModule
+  ]
+})
+export class RegistrosModule { }
