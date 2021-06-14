96852cf80ad2b2756e6c6d45ad40ac768fa97c26
John-Stefan <johnstn1545@hotmail.com>
2021-06-13 22:49:51 -0300

    Implementação do html de cadastro do registro

--- Mobile/bovinainfoMobile/src/app/registros/pages/registro-save/registro-save.module.ts
+++ Mobile/bovinainfoMobile/src/app/registros/pages/registro-save/registro-save.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { RegistroSavePageRoutingModule } from './registro-save-routing.module';
 
 import { RegistroSavePage } from './registro-save.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     RegistroSavePageRoutingModule
   ],
   declarations: [RegistroSavePage]
 })
-export class RegistroSavePageModule {}
+export class RegistroSavePageModule { }
