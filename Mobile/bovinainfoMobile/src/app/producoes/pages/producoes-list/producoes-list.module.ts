723d0800e7d89fc1062b2b2bcb9f8c1ed3502588
John-Stefan <johnstn1545@hotmail.com>
2021-06-14 23:35:26 -0300

    Implementação da tela de listagem de produções

--- Mobile/bovinainfoMobile/src/app/producoes/pages/producoes-list/producoes-list.module.ts
+++ Mobile/bovinainfoMobile/src/app/producoes/pages/producoes-list/producoes-list.module.ts
@@ -3,10 +3,12 @@ import { ProducoesListPageRoutingModule } from './producoes-list-routing.module'
 
 import { ProducoesListPage } from './producoes-list.page';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
     SharedModule,
+    ComponentsModule,
     ProducoesListPageRoutingModule
   ],
   declarations: [ProducoesListPage]
