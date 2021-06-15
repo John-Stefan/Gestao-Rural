693f51fe63d2fc5b1bfa2075647f103b0389b7a7
John-Stefan <johnstn1545@hotmail.com>
2021-06-14 23:56:46 -0300

    Implementação da tela de salvar produções

--- Mobile/bovinainfoMobile/src/app/producoes/pages/producao-save/producao-save.module.ts
+++ Mobile/bovinainfoMobile/src/app/producoes/pages/producao-save/producao-save.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { ProducaoSavePageRoutingModule } from './producao-save-routing.module';
 
 import { ProducaoSavePage } from './producao-save.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     ProducaoSavePageRoutingModule
   ],
   declarations: [ProducaoSavePage]
 })
-export class ProducaoSavePageModule {}
+export class ProducaoSavePageModule { }
