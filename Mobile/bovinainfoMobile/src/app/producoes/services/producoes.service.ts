b119e2f92c6b8cd232cd2d7c46788b68a7a65ce1
John-Stefan <johnstn1545@hotmail.com>
2021-06-14 23:02:00 -0300

    Implementação da camada de serviço da produção

--- Mobile/bovinainfoMobile/src/app/producoes/services/producoes.service.ts
+++ Mobile/bovinainfoMobile/src/app/producoes/services/producoes.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Firestore } from 'src/app/core/classes/firestore.class';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Producao } from '../models/producao.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProducoesService {
+export class ProducoesService extends Firestore<Producao> {
 
-  constructor() { }
+  constructor(private authService: AuthService, db: AngularFirestore) {
+    super(db);
+    this.init();
+  }
+
+  private init(): void {
+    this.authService.authState$.subscribe(user => {
+      if (user) {
+        this.setCollection(`/users/${user.uid}/producoes`);
+        return;
+      }
+      this.setCollection(null);
+    });
+  }
 }
