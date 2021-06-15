532ffbd0739cda8edbbc05cd1e67d92e60abda22
John-Stefan <johnstn1545@hotmail.com>
2021-06-14 23:16:54 -0300

    Correção de imports da produção

--- Mobile/bovinainfoMobile/src/app/producoes/pages/producoes-list/producoes-list.module.ts
+++ Mobile/bovinainfoMobile/src/app/producoes/pages/producoes-list/producoes-list.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { ProducoesListPageRoutingModule } from './producoes-list-routing.module';
 
 import { ProducoesListPage } from './producoes-list.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     ProducoesListPageRoutingModule
   ],
   declarations: [ProducoesListPage]
 })
-export class ProducoesListPageModule {}
+export class ProducoesListPageModule { }
