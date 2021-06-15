0c0899f3536ead7d92e022fd0f69cecf0c89e545
John-Stefan <johnstn1545@hotmail.com>
2021-06-14 21:39:05 -0300

    Correção de imports do modulo das produções

---
+++ Mobile/bovinainfoMobile/src/app/producoes/producoes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ProducoesRoutingModule } from './producoes-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ProducoesRoutingModule
+  ]
+})
+export class ProducoesModule { }
