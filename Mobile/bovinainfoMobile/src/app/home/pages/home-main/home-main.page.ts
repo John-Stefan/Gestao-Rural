24a70eed2ea7dc5bd08ccb92ea4139db08fb6c93
John-Stefan <johnstn1545@hotmail.com>
2021-06-26 12:19:20 -0300

    Implementação do loading na tela home

--- Mobile/bovinainfoMobile/src/app/home/pages/home-main/home-main.page.ts
+++ Mobile/bovinainfoMobile/src/app/home/pages/home-main/home-main.page.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayService } from 'src/app/core/services/overlay.service';
 
 @Component({
   selector: 'app-home-main',
   templateUrl: './home-main.page.html',
   styleUrls: ['./home-main.page.scss'],
 })
-export class HomeMainPage {
+export class HomeMainPage implements OnInit {
 
-  constructor() { }
+  constructor(private overlayService: OverlayService) { }
 
+  async ngOnInit(): Promise<void> {
+    const loading = await this.overlayService.loading({
+      duration: 1500
+    });
+  }
 }
 
 
