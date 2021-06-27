bdc4b11c7a5e09e71a79bf8915121c6454b21f3d
John-Stefan <johnstn1545@hotmail.com>
2021-06-26 21:46:53 -0300

    Implementação da pagina de login

--- Front-End/bovinainfoFront/src/app/auth/pages/login/login.component.ts
+++ Front-End/bovinainfoFront/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email(): FormControl {
+    return <FormControl>this.authForm.get('email');
+  }
+
+  get password(): FormControl {
+    return <FormControl>this.authForm.get('password');
+  }
+
+  onSubmit(): void {
+    console.log('AuthForm: ', this.authForm.value);
+  }
 }
