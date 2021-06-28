092865c4b6ddfb2abb803cbbba41c0fb0bdb5b65
John-Stefan <johnstn1545@hotmail.com>
2021-06-27 22:15:17 -0300

    Implementação se service

--- Front-End/bovinainfoFront/src/app/core/classes/firestore.class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Firestore } from './firestore.class';
-
-describe('Firestore', () => {
-  it('should create an instance', () => {
-    expect(new Firestore()).toBeTruthy();
-  });
-});
--- Front-End/bovinainfoFront/src/app/pessoas/pages/pessoas-list/pessoas-list.component.ts
+++ Front-End/bovinainfoFront/src/app/pessoas/pages/pessoas-list/pessoas-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Pessoa } from '../../models/pessoa.model';
+import { PessoasService } from '../../services/pessoas.service';
 
 @Component({
   selector: 'app-pessoas-list',
@@ -11,25 +12,13 @@ export class PessoasListComponent implements OnInit {
 
   pessoas$: Observable<Pessoa[]>
   pessoa: Pessoa;
+  loading: boolean;
   pessoaDialogEndereco: boolean;
 
-  constructor() { }
+  constructor(private pessoasService: PessoasService) { }
 
   ngOnInit(): void {
-    this.pessoas$ = of([
-      {
-        id: '1',
-        nome: 'John Stefan',
-        cpf: '062.010.001.00',
-        dataNascimento: '08/10/2000',
-        email: 'johnstn1545@gmail.com',
-        telefone: '62981066673',
-        cep: '74474398',
-        logradouro: 'Rua RB52 A',
-        complemento: 'Perto do supermecado martins',
-        numero: '0'
-      }
-    ]);
+    this.pessoas$ = this.pessoasService.getAll();
   }
 
   infoEndereco(pessoa: Pessoa) {
